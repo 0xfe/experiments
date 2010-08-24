18df2e3aabbbe424ea05cdc5bd7c734c0d177fc0
Mohit Cheppudira <mohit@muthanna.com>
2010-08-24 11:15:33 -0400

    Some token-bucket cleanup and refactoring:
    - Added type signatures
    - Added module exports

--- haskell/TokenBucket.hs
+++ haskell/TokenBucket.hs
@@ -1,68 +1,88 @@
 -- Implementation of a simple Token Bucket rate-limiter.
 -- Mohit Cheppudira <mohit@muthanna.com>
 
-module TokenBucket where
+module TokenBucket
+  ( Handle
+  , createHandle
+  , start
+  , stop
+  , getToken
+  , test ) where
 
 import Control.Concurrent
 import Control.Concurrent.MVar
 import Data.Maybe
 
 data Bucket = Bucket {
-        tbTokenCount :: Int,
-        tbRefillCount :: Int,
-        tbThreadID :: Maybe ThreadId
+        tokenCount :: Int,
+        refillCount :: Int,
+        threadID :: Maybe ThreadId
       }
 
-data Handle = Handle (MVar Bucket)
+newtype Handle = Handle (MVar Bucket)
 
-forever a = do a; forever a
-
-init rate = do
+createHandle :: Int -> IO Handle
+createHandle rate = do
   handleMVar <- newEmptyMVar
-  putMVar handleMVar Bucket { tbTokenCount = rate
-                            , tbRefillCount = rate
-                            , tbThreadID = Nothing }
+  putMVar handleMVar Bucket { tokenCount = rate
+                            , refillCount = rate
+                            , threadID = Nothing }
   return $ Handle handleMVar
 
-start (Handle hMVar) = do
-    bucket <- takeMVar hMVar
-    thread_id <- forkIO $ do refillBucket
-    putMVar hMVar (bucket { tbThreadID = Just thread_id })
-  where refillBucket = do
-          threadDelay 1000000
-          bucket <- takeMVar hMVar
-          putMVar hMVar (bucket { tbTokenCount = tbRefillCount bucket })
-          case (tbThreadID bucket) of
-            Nothing -> return ()
-            Just _ -> refillBucket
+start :: Handle -> IO ()
+start (Handle hMVar) = modifyMVar_ hMVar startRefiller
+  where
+    startRefiller bucket = do
+      thread_id <- forkIO refillBucket
+      return bucket { threadID = Just thread_id }
+    refillBucket = do
+      threadDelay 1000000
+      thread_id <- modifyMVar hMVar (\bucket ->
+        return (bucket { tokenCount = refillCount bucket },
+                threadID bucket))
+      case thread_id of
+        Nothing -> return ()
+        Just _ -> refillBucket
 
+stop :: Handle -> IO Bool
 stop (Handle hMVar) = do
   bucket <- takeMVar hMVar
-  case (tbThreadID bucket) of
+  case (threadID bucket) of
     Nothing -> return False
     Just thread_id -> do
-      putMVar hMVar bucket { tbThreadID = Nothing }
+      putMVar hMVar bucket { threadID = Nothing }
       return True
 
+getToken :: Handle -> IO (Maybe Bool)
 getToken (Handle hMVar) = modifyMVar hMVar returnToken
   where returnToken bucket =
-          case (tbThreadID bucket) of
+          case (threadID bucket) of
             Nothing -> return (bucket, Nothing)
             Just thread_id -> do
-              let count = tbTokenCount bucket
+              let count = tokenCount bucket
               if count > 0
-                then return (bucket { tbTokenCount = count - 1 }, Just True)
+                then return (bucket { tokenCount = count - 1 }, Just True)
                 else return (bucket, Just False)
 
+forever :: IO a -> IO ()
+forever a = do a; forever a
+
+test :: IO ()
 test = do
   -- Rate-limit at 5 operations/sec.
-  handle <- TokenBucket.init 5
+  handle <- createHandle 5
+
+  -- Start token refiller
   start $ handle
 
+  -- Stop refiller after 3 seconds
   forkIO $ do threadDelay 3000000 >> stop handle >> return ()
 
-  -- Request a token every 100ms. Only 5 should succeed a second.
-  forever $ do
+  -- Request a token every 100ms
+  forkIO $ forever $ do
     success <- getToken handle
     print success
     threadDelay 100000
+
+  -- Run test for 5 seconds
+  threadDelay 5000000
--- haskell/TokenBucketTest.hs
+++ haskell/TokenBucketTest.hs
@@ -1,3 +1,7 @@
+-- Test harness for TokenBucket module.
+-- Mohit Cheppudira <mohit@muthanna.com>
+
 import TokenBucket
 
+main :: IO ()
 main = TokenBucket.test

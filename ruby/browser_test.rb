0b580f6bf3b31d2df977b5fa4a10e1fda19bea58
Mohit Cheppudira <mohit@muthanna.com>
2011-03-03 14:44:56 -0500

    Added firefox support.

--- ruby/browser_test.rb
+++ ruby/browser_test.rb
@@ -27,21 +27,33 @@ class GoogleSearch
 
     # Match on /Search/ because of Google Instant - its buttons are
     # "Google Search" or "Search"
-    @browser.button(:value => /Search/).click()
+
+    button = @browser.button(:value => /Search/)
+    if not button
+      puts "Error: Couldn't find search button"
+    end
+
+    begin
+      button.click()
+    rescue Selenium::WebDriver::Error::ElementNotDisplayedError
+      puts "Couldn't find click button."
+    end
 
     assert(@browser.text.include?('muthanna'), "No muthanna found")
   end
 
   def test_reddit
     @browser.goto("http://reddit.com/r/programming")
-    assert(@browser.text.include?('PROGRAMMING'), "Not Programming")
+    assert(@browser.text.include?('Programming'), "Not Programming")
   end
 end
 
-browser = Watir::Browser.new(:chrome)
-search = GoogleSearch.new(browser)
+[:chrome, :firefox].each { |browser|
+  b = Watir::Browser.new(browser)
+  search = GoogleSearch.new(b)
 
-search.test_vanity_search
-search.test_reddit
+  search.test_vanity_search
+  search.test_reddit
 
-browser.close
+  b.close
+}

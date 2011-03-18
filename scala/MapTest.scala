8b703348f23c54676c562a3d070a292192460db6
Mohit Cheppudira <mohit@muthanna.com>
2011-03-18 15:07:24 -0400

    Basic golang "Hello World" with waf script.

--- scala/MapTest.scala
+++ scala/MapTest.scala
@@ -1,9 +1,12 @@
 package com.muthanna.playground.scala
 
+import java.lang.Integer;
+import java.util.Map;
+import java.util.HashMap;
+
 object MapTest {
   def main(args: Array[String]) {
-    var capital = Map("US" -> "DC", "India" -> "Delhi")
-    capital += ("Canada" -> "Ottawa")
-    println(capital("India"))
+    var aMap = new HashMap[Integer, String]
+    println("boo");
   }
 }

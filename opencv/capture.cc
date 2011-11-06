b1e4632a8b87560c142ef2daf8888040349d02fe
Mohit Cheppudira <mohit@muthanna.com>
2011-11-06 11:44:20 -0500

    Added first OpenCV example.

---
+++ opencv/capture.cc
@@ -0,0 +1,16 @@
+#include "highgui.h"
+
+int main( int argc, char** argv ) {
+    cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
+    CvCapture* capture = cvCreateFileCapture( argv[1] );
+    IplImage* frame;
+    while(1) {
+        frame = cvQueryFrame( capture );
+        if( !frame ) break;
+        cvShowImage( "Example2", frame );
+        char c = cvWaitKey(33);
+        if( c == 27 ) break;
+    }
+    cvReleaseCapture( &capture );
+    cvDestroyWindow( "Example2" );
+}

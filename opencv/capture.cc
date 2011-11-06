b231ad239a60ba9d70d2c75fea229dda21797307
Mohit Cheppudira <mohit@muthanna.com>
2011-11-06 13:20:13 -0500

    Add capture from camera.

--- opencv/capture.cc
+++ opencv/capture.cc
@@ -6,16 +6,23 @@
  */
 
 int main( int argc, char** argv ) {
-    cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
-    CvCapture* capture = cvCreateFileCapture( argv[1] );
-    IplImage* frame;
-    while(1) {
-        frame = cvQueryFrame( capture );
-        if( !frame ) break;
-        cvShowImage( "Example2", frame );
-        char c = cvWaitKey(33);
-        if( c == 27 ) break;
-    }
-    cvReleaseCapture( &capture );
-    cvDestroyWindow( "Example2" );
+  cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
+  CvCapture* capture;
+
+  if (argc < 2) {
+    capture = cvCreateCameraCapture(0);
+  } else {
+    capture = cvCreateFileCapture( argv[1] );
+  }
+
+  IplImage* frame;
+  while(1) {
+      frame = cvQueryFrame( capture );
+      if( !frame ) break;
+      cvShowImage( "Example2", frame );
+      char c = cvWaitKey(33);
+      if( c == 27 ) break;
+  }
+  cvReleaseCapture( &capture );
+  cvDestroyWindow( "Example2" );
 }

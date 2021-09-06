79356eb59cbc9d22e0d39df5e923c853def3f537
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2021-09-06 19:53:33 -0400

    Add methods to structs.

--- rust/hello/src/structs.rs
+++ rust/hello/src/structs.rs
@@ -4,6 +4,20 @@ struct Color {
     b: u8
 }
 
+impl Color {
+    fn new(r: u8, g: u8, b: u8) -> Color {
+        Color{r: r, g: g, b: b}
+    }
+
+    fn css(&self) -> String {
+        format!("#{:02x}{:02x}{:02x}", self.r, self.g, self.b)
+    }
+
+    fn set_r(&mut self, v: u8) {
+        self.r = v;
+    }
+}
+
 pub fn run() {
     let c = Color{r: 5, g: 6, b: 7};
     println!("{} {} {}", c.r, c.g, c.b);
@@ -12,4 +26,10 @@ pub fn run() {
     struct OtherColor(u8, u8, u8);
     let uc = OtherColor(4, 5, 6);
     println!("{} {} {}", uc.0, uc.1, uc.1);
+
+    let mut mycolor = Color::new(25, 68, 230);
+    println!("{}", mycolor.css());
+
+    mycolor.set_r(200);
+    println!("{}", mycolor.css());
 }

2f48442774dd929f6f5acd60443a8e0c95fa8633
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-01-30 18:33:37 -0500

    Start stop and change speed.

--- emdr/src/index.ts
+++ emdr/src/index.ts
@@ -1,37 +1,52 @@
 function drawCircle() {
     const svgns = "http://www.w3.org/2000/svg";
     const container = document.getElementById( 'cont' );
+    console.log("cont:", container.getBoundingClientRect());
     let circle = document.createElementNS(svgns, 'circle');
     circle.setAttribute("id", "circle1");
     circle.setAttributeNS(null, 'cx', "300");
     circle.setAttributeNS(null, 'cy', "300");
-    circle.setAttributeNS(null, 'r', "50");
-    circle.setAttributeNS(null, 'style', 'fill: none; stroke: blue; stroke-width: 1px;' );
+    circle.setAttributeNS(null, 'r', "30");
+    circle.setAttributeNS(null, 'style', 'fill: red; stroke: gray; stroke-width: 2px;' );
     container.appendChild(circle);
 }
 
-function animate() {
-    let cx = 10;
-    let step = 2;
+function animate(speed: number): NodeJS.Timer  {
+    let cx = 300;
+    let step = speed;
+    const container = document.getElementById( 'cont' );
+    const width = container.getBoundingClientRect().width;
 
     const circle = document.getElementById('circle1');
 
-    setInterval(function () {
+    const timer = setInterval(function () {
         circle.setAttributeNS(null, 'cx', cx.toString())
 
         cx += step;
-        if (cx > 800) {
-            step = -2;
-        } else if (cx < 20) {
-            step = 2;
+        if (cx > width - 100) {
+            step = -speed;
+        } else if (cx < 100) {
+            step = speed;
         }
-    }, 20);
+    }, 5);
+
+    return timer;
 }
 
 function main() {
-  console.log("Hello world!")
+  const step = 10;
   drawCircle();
-  animate();
+  let timer = animate(step);
+
+  const startstop = document.getElementById("startstop");
+  startstop.addEventListener("click", function(event) {
+    if (timer) {
+        clearInterval(timer);
+        timer = null;
+    } else {
+        timer = animate(step);
+    }
+  });
 }
 
 main();

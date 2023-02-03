cb3af179c63ef1f1c188bd46d1aab42ee8b8ac90
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-02-02 22:21:19 -0500

    Encapsulated Circle and Bouncer in classes.

--- emdr/src/index.ts
+++ emdr/src/index.ts
@@ -1,52 +1,101 @@
-function drawCircle() {
-    const svgns = "http://www.w3.org/2000/svg";
-    const container = document.getElementById( 'cont' );
-    console.log("cont:", container.getBoundingClientRect());
-    let circle = document.createElementNS(svgns, 'circle');
-    circle.setAttribute("id", "circle1");
-    circle.setAttributeNS(null, 'cx', "300");
-    circle.setAttributeNS(null, 'cy', "300");
-    circle.setAttributeNS(null, 'r', "30");
-    circle.setAttributeNS(null, 'style', 'fill: red; stroke: gray; stroke-width: 2px;' );
-    container.appendChild(circle);
+class Circle {
+    x: number;
+    y: number;
+    radius: number;
+    style: string;
+    el: SVGElement;
+
+    constructor(x:number, y:number, radius: number) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.style = 'fill: red; stroke: gray; stroke-width: 2px;';
+    }
+
+    setStyle(style: string) {
+        this.style = style;
+    }
+
+    createElement(containerEl: string) {
+        const svgns = "http://www.w3.org/2000/svg";
+        this.el = document.createElementNS(svgns, 'circle');
+        let container = document.getElementById(containerEl);
+        this.el.setAttribute("id", "circle1");
+        this.el.setAttributeNS(null, 'cx', this.x.toString());
+        this.el.setAttributeNS(null, 'cy', this.y.toString());
+        this.el.setAttributeNS(null, 'r', this.radius.toString());
+        this.el.setAttributeNS(null, 'style', this.style);
+        container.appendChild(this.el);
+        return this;
+    }
+
+    moveTo(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.el.setAttributeNS(null, 'cx', x.toString());
+        this.el.setAttributeNS(null, 'cy', y.toString());
+    }
 }
 
-function animate(speed: number): NodeJS.Timer  {
-    let cx = 300;
-    let step = speed;
-    const container = document.getElementById( 'cont' );
-    const width = container.getBoundingClientRect().width;
+class Bouncer {
+    circle: Circle;
+    x: number;
+    minX: number;
+    maxX: number;
+    speed: number;
+    timer: NodeJS.Timer;
+    running: boolean;
 
-    const circle = document.getElementById('circle1');
+    constructor(circle: Circle) {
+        this.circle = circle;
+        this.x = circle.x;
+        this.minX = circle.x;
+        this.maxX = circle.x + 20;
+        this.speed = 20;
+        this.running = false;
+    }
 
-    const timer = setInterval(function () {
-        circle.setAttributeNS(null, 'cx', cx.toString())
+    setBounds(minX: number, maxX: number) {
+        this.minX = minX;
+        this.maxX = maxX;
+    }
 
-        cx += step;
-        if (cx > width - 100) {
-            step = -speed;
-        } else if (cx < 100) {
-            step = speed;
-        }
-    }, 5);
+    start() {
+        let step = this.speed;
+        const that = this;
+        const interval_ms = 5;
+        this.timer = setInterval(function() {
+            that.circle.moveTo(that.x, that.circle.y);
+            that.x += step;
+            if (that.x > that.maxX) {
+                step = -that.speed;
+            } else if (that.x < that.minX) {
+                step = that.speed;
+            }
+        }, interval_ms);
+        this.running = true;
+    }
 
-    return timer;
+    stop() {
+        clearInterval(this.timer);
+        this.running = false;
+    }
+
+    toggle() {
+        this.running ? this.stop() : this.start();
+    }
 }
 
 function main() {
-  const step = 10;
-  drawCircle();
-  let timer = animate(step);
+    const container = document.getElementById( 'cont' );
+    const width = container.getBoundingClientRect().width;
+    const bouncer = new Bouncer(new Circle(300, 300, 30).createElement("cont"));
+    bouncer.setBounds(100, width - 100);
 
-  const startstop = document.getElementById("startstop");
-  startstop.addEventListener("click", function(event) {
-    if (timer) {
-        clearInterval(timer);
-        timer = null;
-    } else {
-        timer = animate(step);
-    }
-  });
+    const startstop = document.getElementById("startstop");
+    startstop.addEventListener("click", function(event) {
+        bouncer.toggle();
+    });
 }
 
 main();

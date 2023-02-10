ef79c536a441019ab299c1d1ce3ee72b045393ea
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-02-10 09:00:46 -0500

    Added TcpListener to server.

--- rust/hello/src/server.rs
+++ rust/hello/src/server.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+
+fn start() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        println!("Connection established: {:?}", stream)
+    }
+}
+
 pub fn run() {
-    println!("starting server...")
+    println!("starting server...");
+    start();
 }

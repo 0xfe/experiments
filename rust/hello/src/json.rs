0ce8f8bbb9ecb3301005a3524025275438bd80aa
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-02-11 10:57:40 -0500

    Added serialize/deserialize methods.

--- rust/hello/src/json.rs
+++ rust/hello/src/json.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
 
 fn parse_string() -> Result<()> {
@@ -19,6 +20,36 @@ fn parse_string() -> Result<()> {
     Ok(())
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Address {
+    street: String,
+    city: String,
+}
+
+fn deserialize() {
+    let data = r#"
+        {
+            "street": "21 Downing St.",
+            "city": "London"
+        }
+    "#;
+
+    let v: Address = serde_json::from_str(data).unwrap();
+    println!("{:?}", v);
+}
+
+fn serialize() {
+    let address = Address {
+        street: "555 Prospect Pl.".into(),
+        city: "Brooklyn".into(),
+    };
+
+    let data = serde_json::to_string(&address).unwrap();
+    println!("{:?}", data);
+}
+
 pub fn run() {
     parse_string().unwrap();
+    deserialize();
+    serialize();
 }

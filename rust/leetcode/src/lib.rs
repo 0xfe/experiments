17c38dc327203d011f01a592f7d00c5b377447d3
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-09-09 16:26:01 -0400

    Finished longest substring

--- rust/leetcode/src/lib.rs
+++ rust/leetcode/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod add_two_numbers;
+pub mod longest_substring_without_repeating_characters;
 pub mod two_sum;
---
+++ rust/leetcode/src/longest_substring_without_repeating_characters.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+pub fn length_of_longest_substring(s: String) -> i32 {
+    let mut longest = 0;
+    let mut current = 0;
+
+    let mut set = HashMap::new();
+
+    println!("\n{:?}", s);
+
+    for (i, c) in s.chars().enumerate() {
+        println!("i: {} c: {} current: {} set: {:?}", i, c, current, set);
+        if let Some(v) = set.get(&c) {
+            current = i - v;
+            let t = *v;
+            set.retain(|_, val| *val >= t);
+            set.insert(c, i);
+        } else {
+            set.insert(c, i);
+            current += 1;
+        }
+
+        if current > longest {
+            longest = current;
+        }
+    }
+
+    longest as i32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(length_of_longest_substring("abcabcbb".to_string()), 3);
+        assert_eq!(length_of_longest_substring("bbbbb".to_string()), 1);
+        assert_eq!(length_of_longest_substring("pwwkew".to_string()), 3);
+        assert_eq!(length_of_longest_substring("".to_string()), 0);
+        assert_eq!(length_of_longest_substring(" ".to_string()), 1);
+        assert_eq!(length_of_longest_substring("au".to_string()), 2);
+        assert_eq!(length_of_longest_substring("dvdf".to_string()), 3);
+        assert_eq!(length_of_longest_substring("abba".to_string()), 2);
+        assert_eq!(length_of_longest_substring("tmmzuxt".to_string()), 5);
+    }
+}

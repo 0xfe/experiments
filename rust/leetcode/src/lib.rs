453f9634837f367850f0496fa39286d43e2aec51
Mohit Muthanna Cheppudira <mohit@muthanna.com>
2023-09-17 09:40:32 -0400

    Finished reverse integer

--- rust/leetcode/src/lib.rs
+++ rust/leetcode/src/lib.rs
@@ -2,5 +2,6 @@ pub mod add_two_numbers;
 pub mod longest_palindromic_substring;
 pub mod longest_substring_without_repeating_characters;
 pub mod median_of_two_sorted_arrays;
+pub mod reverse_integer;
 pub mod two_sum;
 pub mod zigzag_conversion;
---
+++ rust/leetcode/src/reverse_integer.rs
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/reverse-integer/
+
+#[allow(clippy::result_unit_err)]
+pub fn reverse2(x: i32) -> Result<i32, ()> {
+    println!("\nx: {}", x);
+    let mut result = 0i32;
+    let mut started = None;
+
+    let mut final_place = false;
+    for i in (1..10).rev() {
+        let place = 10i32.checked_pow(i).ok_or(())?;
+        let digit = (x % place) / 10i32.pow(i - 1);
+
+        if i == 9 && x / place != 0 {
+            final_place = true;
+        }
+
+        if digit != 0 && started.is_none() {
+            started = Some(i);
+        }
+
+        if let Some(started) = started {
+            let multiple = digit
+                .checked_mul(10i32.checked_pow(started - i).ok_or(())?)
+                .ok_or(())?;
+
+            result = result.checked_add(multiple).ok_or(())?;
+        }
+    }
+
+    if final_place {
+        result = result.checked_mul(10).ok_or(())?;
+    }
+
+    Ok(result)
+}
+
+pub fn reverse(x: i32) -> i32 {
+    reverse2(x).unwrap_or(0)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_7() {
+        assert_eq!(reverse(123), 321);
+        assert_eq!(reverse(-123), -321);
+        assert_eq!(reverse(120), 21);
+        assert_eq!(reverse(102), 201);
+        assert_eq!(reverse(-1002), -2001);
+        assert_eq!(reverse(0), 0);
+        assert_eq!(reverse(1_534_236_469), 0);
+        assert_eq!(reverse(-1_534_236_469), 0);
+    }
+}
